bb1398d4696768a2863b2089d3c17755118b24a4
Pawel Korus <pkorus@gmail.com>
2019-02-26 14:14:46 -0500

    Refactor and added support for directory wildcards

--- summarize_nip.py
+++ summarize_nip.py
@@ -1,29 +1,56 @@
 from collections import defaultdict
 import json
-import sys
 import os
+import sys
+import argparse
 
-dirname = './data/raw/nip_model_snapshots/'
 
-cameras = sorted(os.listdir(dirname))
+def mean(x):
+    return sum(x) / len(x)
 
-print('Showing stats for {}'.format(dirname))
 
-stats = {'psnr': defaultdict(lambda: []), 'ssim': defaultdict(lambda: [])}
+def summarize_results(dirname, show_stats=True, n=1):
 
-for camera in cameras:
-    print('\n', camera)
-    pipelines = sorted(os.listdir(os.path.join(dirname, camera)))
-    for pipe in pipelines:
-        with open(os.path.join(dirname, camera, pipe, 'progress.json')) as f:
-            ts = json.load(f)
-        print('  {:10s} -> PSNR: {:.2f} dB\tSSIM: {:.3f}'.format(pipe, ts['psnr'][-1], ts['ssim'][-1]))
+    cameras = sorted(os.listdir(dirname))
+
+    print('\n# {}'.format(dirname))
+
+    stats = {'psnr': defaultdict(lambda: []), 'ssim': defaultdict(lambda: [])}
+
+    for camera in cameras:
+        print('\n  {}'.format(camera))
+        pipelines = sorted(os.listdir(os.path.join(dirname, camera)))
+
+        for pipe in pipelines:
+            with open(os.path.join(dirname, camera, pipe, 'progress.json')) as f:
+                ts = json.load(f)
+            data = ts if 'psnr' in ts else ts['Performance']
+
+            print('    {:10s} -> PSNR: {:.2f} dB\tSSIM: {:.3f}'.format(pipe, mean(data['psnr'][-n:]), mean(data['ssim'][-n:])))
+
+            for k, v in stats.items():
+                v[pipe].append(mean(data[k][-n:]))
+
+    if show_stats:
         for k, v in stats.items():
-            v[pipe].append(ts[k][-1])
+            print('\n  ! Mean {}'.format(k))
+            for pipe in v.keys():
+                print('    {:10s}: {:.3f}'.format(pipe, sum(v[pipe]) / len(v[pipe])))
 
-for k, v in stats.items():
-    print('\n# Mean {}'.format(k))
-    for pipe in v.keys():
-        print('{:10s}: {:.3f}'.format(pipe, sum(v[pipe]) / len(v[pipe])))
 
-    # print(v)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Summarize results of NIP training')
+    parser.add_argument('dirs', nargs='*', default=['./data/raw/nip_model_snapshots/'])
+    parser.add_argument('--stats', dest='stats', action='store_true', default=False,
+                        help='Display summary stats')
+    parser.add_argument('--n', dest='n', action='store', default=1, type=int,
+                        help='Set > 1 to average last N samples')
+
+    args = parser.parse_args()
+
+    for dirname in args.dirs:
+        if os.path.exists(dirname):
+            summarize_results(dirname, args.stats, args.n)
+        else:
+            print('Error: directory {} does not exist!'.format(os.path.abspath(dirname)))
+            sys.exit(1)
