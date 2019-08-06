59d4a9552a5b0207c4d20b3c16405131e54f3cb5
Paweł Korus <pkorus@gmail.com>
2019-08-06 12:15:52 -0400

    Added CLI parameter

--- train_manipulation_batch.sh
+++ train_manipulation_batch.sh
@@ -6,9 +6,15 @@ if [ "$USER" = "pk91" ]; then
     source ../neural-imaging-pipeline/venv/bin/activate
 fi
 
+if [ -n "$3" ]; then
+  rep=$3
+else
+  rep=5
+fi
+
 nip="DNet"
 cam="D90"
-cmd="python3 train_manipulation.py --end 10 --patch 128 --epochs=4001 --ds none --nip $nip --cam $cam"
+cmd="python3 train_manipulation.py --end $rep --patch 128 --epochs=4001 --ds none --nip $nip --cam $cam"
 
 if [ "$2" = "dry" ]; then
     cmd="echo $cmd"
@@ -17,12 +23,12 @@ fi
 # Scope of parameters for exploration
 # ln="--ln 0.2 --ln 0.1 --ln 0.05 --ln 0.01 --ln 0.005 --ln 0.001"
 ln="--ln 0.5 --ln 0.4 --ln 0.3 --ln 0.2 --ln 0.1 --ln 0.075 --ln 0.05 --ln 0.01"
-lc="--lc 1.0 --lc 0.5 --lc 0.1 --lc 0.05 --lc 0.01 --lc 0.005 --lc 0.001"
+lc="--lc 10.0 --lc 5.0 --lc 1.0 --lc 0.5 --lc 0.1 --lc 0.05 --lc 0.01 --lc 0.005 --lc 0.001"
 
 case "$1" in
     jpeg)
         # Fixed JPEG Experiments
-        for jpeg in 10 20 30 35 40 45 50 55 60 65 70 75 80 85 90 95; do
+        for jpeg in 10 15 20 25 30 35 40 45 50 55 60 65 70 75 80 85 90 95; do
             $cmd --dir ./data/raw/m/jpeg/$jpeg --jpeg $jpeg
         done
         ;;
