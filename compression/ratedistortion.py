883ffb5f9c1f3dcddb234ab67d0772f53f19c4a7
Paweł Korus <pkorus@gmail.com>
2019-08-15 09:11:34 -0400

    Ported BPG format support from imgcomp-cvpr @ github

--- compression/ratedistortion.py
+++ compression/ratedistortion.py
@@ -15,7 +15,7 @@ import uncertainties.unumpy as unp
 import uncertainties as unc
 
 from helpers import loading, utils, coreutils
-from compression import jpeg_helpers, afi
+from compression import jpeg_helpers, afi, bpg_helpers
 
 
 def get_jpeg_df(directory, write_files=False, effective_bytes=True, force_calc=False):
@@ -135,10 +135,71 @@ def get_jpeg2k_df(directory, write_files=False, effective_bytes=True, force_calc
                         pbar.set_postfix(image_id=image_id, quality=q)
                         pbar.update(1)
 
-            df.to_csv(os.path.join(directory, 'jpeg2000.csv'), index=False)
+            df.to_csv(df_jpeg_path, index=False)
 
         return df
 
+
+def get_bpg_df(directory, write_files=False, effective_bytes=True, force_calc=False):
+
+    files, _ = loading.discover_files(directory, n_images=-1, v_images=0)
+    batch_x = loading.load_images(files, directory, load='y')
+    batch_x = batch_x['y'].astype(np.float32) / (2 ** 8 - 1)
+
+    # Get trade-off for JPEG
+    quality_levels = np.arange(1, 51, 1)
+    df_jpeg_path = os.path.join(directory, 'bpg.csv')
+
+    if os.path.isfile(df_jpeg_path) and not force_calc:
+        print('Restoring BPG stats from {}'.format(df_jpeg_path))
+        df = pd.read_csv(df_jpeg_path, index_col=False)
+    else:
+        df = pd.DataFrame(columns=['image_id', 'filename', 'codec', 'quality', 'ssim', 'psnr', 'bytes', 'bpp'])
+
+        with tqdm.tqdm(total=len(files) * len(quality_levels), ncols=120, desc='BPG') as pbar:
+
+            for image_id, filename in enumerate(files):
+
+                # Read the original image
+                image = batch_x[image_id]
+
+                for qi, q in enumerate(quality_levels):
+
+                    # Compress to BPG
+                    # Save as temporary file
+                    imageio.imwrite('/tmp/image.png', (255*image).astype(np.uint8))
+                    bpp_path = bpg_helpers.bpg_compress('/tmp/image.png', q, '/tmp')
+                    bpp = bpg_helpers.bpp_of_bpg_image(bpp_path)
+                    image_bytes = os.stat(bpp_path).st_size
+                    image_compressed = imageio.imread(bpg_helpers.decode_bpg_to_png(bpp_path))
+
+                    # Compress images and get effective bytes (only image data - no headers)
+                    image_compressed, image_bytes = jpeg_helpers.compress_batch(image, q, effective=effective_bytes)
+
+                    if write_files:
+                        image_dir = os.path.join(directory, os.path.splitext(filename)[0])
+                        if not os.path.isdir(image_dir):
+                            os.makedirs(image_dir)
+
+                        image_path = os.path.join(image_dir, 'bpg_q{:03d}.png'.format(q))
+                        imageio.imwrite(image_path, (255 * image_compressed).astype(np.uint8))
+
+                    df = df.append({'image_id': image_id,
+                                    'filename': filename,
+                                    'codec': 'bpg',
+                                    'quality': q,
+                                    'ssim': compare_ssim(image, image_compressed, multichannel=True, data_range=1),
+                                    'psnr': compare_psnr(image, image_compressed, data_range=1),
+                                    'bytes': image_bytes,
+                                    'bpp': bpp
+                                    }, ignore_index=True)
+
+                    pbar.set_postfix(image_id=image_id, quality=q)
+                    pbar.update(1)
+
+        df.to_csv(df_jpeg_path, index=False)
+
+    return df
 
 def get_dcn_df(directory, model_directory, write_files=False, force_calc=False):
 
